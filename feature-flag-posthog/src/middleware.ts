a965606e3e75e5b2ca66dd57c2a6710184e6d41b
Ohansemmanuel <Ohansemmanuel@outlook.com>
2023-09-12 15:29:06 +0200

    ✨ feat: complete annotated posthog middleware

--- feature-flag-posthog/src/middleware.ts
+++ feature-flag-posthog/src/middleware.ts
@@ -2,29 +2,62 @@ import { defineMiddleware } from "astro:middleware";
 
 import { PostHog } from "posthog-node";
 
+const HOME_PAGE_PATH = "/";
+
+/**
+ * Create the Post hog client and pass your project API key
+ * Add POSTHOG_CLIENT to "src/.env"
+ */
 const client = new PostHog(import.meta.env.POSTHOG_CLIENT);
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  console.log("IN MIDDLEWARE");
+  /**
+   * Early return. We will only check the feature flag with requests
+   * to the homepage
+   */
+  if (context.url.pathname !== HOME_PAGE_PATH) {
+    return next();
+  }
 
   try {
+    /**
+     * Retrieve the feature toggle for your feature flag
+     * In this case, "astro-middleware-demo"
+     */
     const isEnabled = await client.isFeatureEnabled(
       "astro-middleware-demo",
-      "static-user-id"
+      "" // Pass a static-user-id
     );
 
     console.log({ isEnabled });
 
     if (isEnabled) {
       console.log("Feature is ENABLED!");
+
+      /**
+       * When the feature flag is toggled on, redirect users who access the homepage,
+       * to the "/post-hog" page
+       */
+      return Response.redirect(new URL("/post-hog", context.url), 302);
+
+      /**
+       * Otherwise, handle the request as usual
+       */
       return next();
-      // Redirect this user to a new page
-      //   return context.redirect("/growth-book", 304);
     }
+
+    /**
+     * Feature flag NOT enabled? Handle the request as usual
+     */
+
+    console.log("Feature is DISABLED!");
+    return next();
   } catch (error) {
     console.error("Failed to load feature flag from PostHog");
-  }
 
-  console.log("Feature is DISAAABLED!");
-  return next();
+    /**
+     * Handle the request as usual
+     */
+    return next();
+  }
 });
